a0335d04daa768b57814f449528d7cba5b268b15
Anna Clara Lucena Braga <annalbraga@hotmail.com>
2026-04-28 20:24:34 -0300

    resolução avaliação 1

--- avaliacoes/01-avaliacao/01-avaliacao-resolucao.R
+++ avaliacoes/01-avaliacao/01-avaliacao-resolucao.R
@@ -52,13 +52,43 @@ caminho_agencias <- here("dados/brutos/agencias.csv")
   
   # Exercício 2 ----------------------------------------------------------
 
-# 2.a)
-
+# 2.a) #Filtre as agências cujo tipo_agencia seja "Plena", salve o resultado 
+#em dados_agencias_plenas e imprima no console.
+  
+  # pipeline de preparação dos dados
+  dados_agencias_limpos <- dados_agencias
+  
+  glimpse (dados_agencias_limpos)
+  
+  
+#filtra os dados de agencias plenas
+  dados_agencias_plenas|> 
+    filter(tipo_agencia == "Plena")
+  
+  #salva o resultado em um novo objeto 
+  dados_agencias_plenas <- dados_agencias_limpos |>
+    filter(tipo_agencia == "Plena")
+  
+  #exibe o resultado 
+  dados_agencias_plenas
+  
 
 # 2.b)
+#Selecione apenas as colunas nome_agencia, cidade e num_cooperados e organiza 
+#ordem decrescente
+  
+  dados_agencias_limpos |>
+    select(nome_agencia, cidade, num_cooperados)|> 
+    arrange(desc(num_cooperados))
 
 
 # 2.c)
+#Filtre as agências localizadas em "Divinópolis" e que possuam mais de 1.000
+#cooperados.
+  
+  dados_agencias_limpos|>
+    filter(cidade == "Divinópolis")|> 
+    filter(num_cooperados>1000)
 
 
 
@@ -67,32 +97,71 @@ caminho_agencias <- here("dados/brutos/agencias.csv")
 # 3.a) pivot_longer
 
 # reorganiza os dados de crédito em trimestre e volume_credito
-dados_credito_longo <- 
+  dados_credito_longo <- dados_credito |> 
+    pivot_longer(
+      cols = c("T1","T2","T3","T4"),
+      names_to = "trimestre",
+      values_to = "volume_credito")
+  
+  #exibe o resultado 
+  dados_credito_longo
+    
+
   
   # 3.b) left_join
   
   # combina `dados_credito_longo`com `dados_agencias`
-  dados_completos <-
+  dados_completos <- dados_credito_longo |> 
+    left_join(dados_agencias, by = "codigo_agencia")
+  
+  #exibe o resultado 
+  dados_completos
   
   
   
   # Exercício 4 ---------------------------------------------------------
 
 # cria dados_analise com credito_por_cooperado
-dados_analise <- 
+  dados_analise <- dados_completos |>
+    mutate(credito_por_cooperado = volume_credito/num_cooperados)
+  
+  
+  #exibe o resultado 
+  dados_analise 
   
   # resume por cidade e ordena por volume_total
-  dados_analise |>
+  dados_analise |> 
+    group_by(cidade) |> 
+    summarise(volume_total = sum(volume_credito),
+    media_dos_creditos_por_cooperado = mean(credito_por_cooperado))|> 
+    arrange(desc(volume_total))
+  
+  #exibe o resultado 
+  dados_analise
   
   
   # Resposta do Exercício 4:
   
-  # Cidade com maior volume_total:
-  # Cidade com maior media_dos_creditos_por_cooperado:
+  # Cidade com maior volume_total: Divinópolis
+  # Cidade com maior media_dos_creditos_por_cooperado: Formiga
   
   
   
   # Exercício 5 ---------------------------------------------------------
 
 # classifica nivel_credito e resume por tipo_agencia
-resumo_por_tipo <- 
+resumo_por_tipo <- dados_analise |>
+    mutate(
+      nivel_credito = case_when(
+        credito_por_cooperado < 1400 ~ "Baixo",
+        credito_por_cooperado >= 1400 & volume_credito < 1700 ~ "Médio",
+        credito_por_cooperado >= 1700 ~ "Alto"
+      )
+    ) |>
+    group_by(tipo_agencia, nivel_credito) |>
+    summarise(volume_total = sum (volume_credito),
+              n_obs = n(3)
+    )|>
+    arrange(desc(volume_total))
+  
+  
